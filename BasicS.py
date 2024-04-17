a3f01718279120c639a3340c3447c4ffef2616c7
Kharthik_DMC <karthikofficialcs@gmail.com>
2024-04-17 14:58:26 +0530

    Basics

---
+++ BasicS.py
@@ -0,0 +1,7 @@
+#Module -> that contains the python code,function,class,object to used this in the other program
+#how to use the module
+def Display():
+    print("Cristiano Ronaldo")
+def Run():
+    print("The run of the team is 197")
+
