ad23b61f716332062e16204a04f9d208f06a8a98
Kharthik_DMC <karthikofficialcs@gmail.com>
2024-04-17 15:03:23 +0530

    Second Edit

---
+++ Start.py
@@ -0,0 +1,45 @@
+# ->comment
+# Print() Statement
+print(12) # this line print number 12
+print('Kharthik')
+#Variable
+name = "Kharthik" #string
+print(name)
+age = 19 #integer
+print(age)
+#print("My name is"+name+" "+"my age is "+str(age)) #+ operator
+print("my name is",name,"age",age) #-> , operator
+#type casting:
+number = 121 #int
+number1 = float(number) #int -> float
+print(type(number))
+print(type(number1))
+#input from user
+name2 = input("enter the name ") #string
+print(name2)
+
+marks = int(input("enter number"))
+
+print(marks)
+def king():
+    print("ragul")
+king()
+king()
+king()
+def animal(variable1,variable2):
+    print(variable1+variable2)
+animal(12,12)
+
+for i in range(0,10): #range()
+    print(i)
+list = [1,2,3,3,4]
+for j in list:
+    print(j)
+n=0
+while n<10 :
+    print("hi")
+    n+=1
+tuple = (1,2,3,4,5,6)
+print(tuple)
+print(type(tuple))
+
